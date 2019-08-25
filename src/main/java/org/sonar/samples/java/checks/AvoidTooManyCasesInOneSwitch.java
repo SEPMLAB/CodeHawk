239667f510d4e9a4afabf36374b831d464a00ba8
Yolok <jkluio785636@gmail.com>
2019-08-25 22:59:42 +0800

    annotation

--- src/main/java/org/sonar/samples/java/checks/AvoidTooManyCasesInOneSwitch.java
+++ src/main/java/org/sonar/samples/java/checks/AvoidTooManyCasesInOneSwitch.java
@@ -27,87 +27,91 @@ public class AvoidTooManyCasesInOneSwitch extends IssuableSubscriptionVisitor {
 
 	@Override
 	public List<Tree.Kind> nodesToVisit() {
-		return Collections.singletonList(Tree.Kind.METHOD);
+		return Collections.singletonList(Tree.Kind.CLASS);//check every class
 	}
 
 	@Override
 	public void visitNode(Tree tree) {
-		MethodTree methodTree = (MethodTree) tree;
-		BlockTree blockTree = methodTree.block();
-		List<StatementTree> list = blockTree.body();
-		checkStatementTree(list);
+		ClassTree classTree = (ClassTree) tree;
+		List<Tree> treeList = classTree.members();
+		for (Tree tempTree : treeList) {
+			if (tempTree.is(Tree.Kind.METHOD) || tempTree.is(Tree.Kind.CONSTRUCTOR)) {//check every method(constructor) in this class
+				MethodTree methodTree = (MethodTree) tempTree;
+				BlockTree blockTree = methodTree.block();
+				List<StatementTree> list = blockTree.body();
+				checkStatementTree(list);
+			}
+		}
 	}
 
 	private void checkStatementTree(List<StatementTree> list) {
 		for (StatementTree statementTree : list) {
 			switch (statementTree.kind()) {
 
-			case SWITCH_STATEMENT:
+			case SWITCH_STATEMENT://get switch statement in this method
 				SwitchStatementTree switchStatementTree = (SwitchStatementTree) statementTree;
-				int count = countSwitch(switchStatementTree);
-				if (count >= 10)
-					reportIssue(switchStatementTree, "Too many cases in this switch statement !");
+				if (countSwitch(switchStatementTree) >= 10)//check this switch statement, if >= 10 cases(default)
+					reportIssue(switchStatementTree, "Too many cases in this switch statement !");//show this smell
 				break;
 
-			case IF_STATEMENT:
-				StatementTree statementTree2 = ((IfStatementTree) statementTree).thenStatement();
-				checkInnerStatementTree(statementTree2);
+			case IF_STATEMENT://get if statement in this method
+				IfStatementTree ifStatementTree = (IfStatementTree) statementTree;
+				checkInnerStatementTree(ifStatementTree.thenStatement());
 
-				IfStatementTree ifStatementTree = ((IfStatementTree) statementTree);
-				checkElseIfStatementTree(ifStatementTree);
+				checkElseIfStatementTree(ifStatementTree);//check else_if part in this if statement
 				break;
 
-			case WHILE_STATEMENT:
-				StatementTree statementTree6 = ((WhileStatementTree) statementTree).statement();
-				checkInnerStatementTree(statementTree6);
+			case WHILE_STATEMENT://get while statement in this method
+				WhileStatementTree whileStatementTree = (WhileStatementTree) statementTree;
+				checkInnerStatementTree(whileStatementTree.statement());
 				break;
 
-			case FOR_STATEMENT:
-				StatementTree statementTree7 = ((ForStatementTree) statementTree).statement();
-				checkInnerStatementTree(statementTree7);
+			case FOR_STATEMENT://get for statement in this method
+				ForStatementTree forStatementTree = (ForStatementTree) statementTree;
+				checkInnerStatementTree(forStatementTree.statement());
 				break;
 
-			case DO_STATEMENT:
-				StatementTree statementTree8 = ((DoWhileStatementTree) statementTree).statement();
-				checkInnerStatementTree(statementTree8);
+			case DO_STATEMENT://get do_while statement in this method
+				DoWhileStatementTree doWhileStatementTree = (DoWhileStatementTree) statementTree;
+				checkInnerStatementTree(doWhileStatementTree.statement());
 				break;
 
-			case FOR_EACH_STATEMENT:
-				StatementTree statementTree9 = ((ForEachStatement) statementTree).statement();
-				checkInnerStatementTree(statementTree9);
+			case FOR_EACH_STATEMENT://get for_each statement in this method
+				ForEachStatement forEachStatement = (ForEachStatement) statementTree;
+				checkInnerStatementTree(forEachStatement.statement());
 				break;
 
-			case LABELED_STATEMENT:
+			case LABELED_STATEMENT://get labeled statement in this method
 				LabeledStatementTree labeledStatementTree = (LabeledStatementTree) statementTree;
-				StatementTree statementTree10 = labeledStatementTree.statement();
 				List<StatementTree> list2 = new ArrayList<>();
-				list2.add(statementTree10);
+				list2.add(labeledStatementTree.statement());
 				checkStatementTree(list2);
 				break;
 
-			case SYNCHRONIZED_STATEMENT:
-				BlockTree blockTree = ((SynchronizedStatementTree) statementTree).block();
-				checkInnerBlockTree(blockTree);
+			case SYNCHRONIZED_STATEMENT://get synchronized statement in this method
+				SynchronizedStatementTree synchronizedStatementTree = (SynchronizedStatementTree) statementTree;
+				checkInnerBlockTree(synchronizedStatementTree.block());
 				break;
 
-			case TRY_STATEMENT:
-				BlockTree blockTree2 = ((TryStatementTree) statementTree).block();
-				checkInnerBlockTree(blockTree2);
-				List<CatchTree> list3 = ((TryStatementTree) statementTree).catches();
+			case TRY_STATEMENT://get try_catch statement in this method
+				TryStatementTree tryStatementTree = (TryStatementTree) statementTree;//check try part in this try statement
+				checkInnerBlockTree(tryStatementTree.block());
+
+				List<CatchTree> list3 = tryStatementTree.catches();//check catch part in this try statement
 				for (CatchTree catchtree : list3) {
-					BlockTree blockTree3 = catchtree.block();
-					checkInnerBlockTree(blockTree3);
+					checkInnerBlockTree(catchtree.block());
 				}
-				BlockTree blockTree4 = ((TryStatementTree) statementTree).finallyBlock();
-				checkInnerBlockTree(blockTree4);
+
+				checkInnerBlockTree(tryStatementTree.finallyBlock());//check finally part in this try statement
 				break;
 
-			case CLASS:
-				List<Tree> list5 = ((ClassTree) statementTree).members();
-				for (Tree tree : list5) {
-					if (tree.is(Tree.Kind.CONSTRUCTOR)) {
-						BlockTree blockTree5 = ((MethodTree) tree).block();
-						checkInnerBlockTree(blockTree5);
+			case CLASS://get another class statement in this method
+				ClassTree classTree = (ClassTree) statementTree;
+				List<Tree> list4 = classTree.members();
+				for (Tree tree : list4) {
+					if (tree.is(Tree.Kind.CONSTRUCTOR)) {//check constructor part in this class
+						MethodTree methodTree = (MethodTree) tree;
+						checkInnerBlockTree(methodTree.block());
 					}
 				}
 				break;
@@ -118,32 +122,31 @@ public class AvoidTooManyCasesInOneSwitch extends IssuableSubscriptionVisitor {
 		}
 	}
 
-	private void checkElseIfStatementTree(IfStatementTree tree) {
+	private void checkElseIfStatementTree(IfStatementTree tree) {//check inner else_if statement method
 		if (tree.elseKeyword() != null) {
-			StatementTree statementTree =  tree.elseStatement();
+			StatementTree statementTree = tree.elseStatement();
 			if (statementTree.is(Tree.Kind.IF_STATEMENT)) {
 				IfStatementTree ifStatementTree = (IfStatementTree) statementTree;
 				checkElseIfStatementTree(ifStatementTree);
-				StatementTree statementTree2 =  ifStatementTree.thenStatement();
-				checkInnerStatementTree(statementTree2);
-			}else {
+				checkInnerStatementTree(ifStatementTree.thenStatement());
+			} else {
 				checkInnerStatementTree(statementTree);
 			}
 		}
 	}
 
-	private void checkInnerStatementTree(StatementTree tree) {
+	private void checkInnerStatementTree(StatementTree tree) {//check inner statementtree method
 		BlockTree blockTree = (BlockTree) tree;
 		List<StatementTree> list = blockTree.body();
 		checkStatementTree(list);
 	}
 
-	private void checkInnerBlockTree(BlockTree tree) {
+	private void checkInnerBlockTree(BlockTree tree) {//check inner blocktree method
 		List<StatementTree> list = tree.body();
 		checkStatementTree(list);
 	}
 
-	private int countSwitch(SwitchStatementTree temp) {
+	private int countSwitch(SwitchStatementTree temp) {//count how many cases in this switch statement method 
 		List<CaseGroupTree> tempList = temp.cases();
 		return tempList.size();
 	}
