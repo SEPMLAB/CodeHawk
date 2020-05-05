2a734a77fca97d1672e21a7de5fca9daa91ba64d
jason30704 <jason30704@gmail.com>
2020-05-05 21:51:07 +0800

    add InappropriateIntimacy

--- src/main/java/org/codehawk/plugin/java/checks/InappropriateIntimacy.java
+++ src/main/java/org/codehawk/plugin/java/checks/InappropriateIntimacy.java
@@ -2,10 +2,21 @@ package org.codehawk.plugin.java.checks;
 
 import java.util.Collections;
 import java.util.List;
+
+import org.codehawk.smell.modler.PODetector;
+import org.codehawk.smell.modler.POMethodNode;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.semantic.Type;
+import org.sonar.plugins.java.api.tree.BlockTree;
+import org.sonar.plugins.java.api.tree.ClassTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonar.plugins.java.api.tree.IdentifierTree;
 import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Modifier;
+import org.sonar.plugins.java.api.tree.ModifierKeywordTree;
+import org.sonar.plugins.java.api.tree.ReturnStatementTree;
+import org.sonar.plugins.java.api.tree.StatementTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
 @Rule(key = "InappropriateIntimacy")
@@ -17,30 +28,51 @@ public class InappropriateIntimacy extends IssuableSubscriptionVisitor {
 	@Override
 	public List<Tree.Kind> nodesToVisit() {
 		// Register to the kind of nodes you want to be called upon visit.
-		return Collections.singletonList(Tree.Kind.METHOD);
+		return Collections.singletonList(Tree.Kind.CLASS);
 	}
 
 	@Override
 	public void visitNode(Tree tree) {
-
-		MethodTree mt = (MethodTree) tree;
-		List<IdentifierTree> trees = mt.symbol().usages();
-		System.out.println("use:" + mt.symbol().usages());
-		System.out.println("num:" + trees.size());
-		for (Tree target : trees) {
-			while (target.parent() != null) {
-				
-				if(target.parent().is(Tree.Kind.CLASS)) {
-					System.out.println("use in:" + target.parent());
-					break;
-				} else {
-					target = target.parent();
-				}		
+		String className;
+		
+		ClassTree ct = (ClassTree) tree;
+		className = ct.simpleName().name();
+		
+		List<Tree> members = ct.members();
+		for (Tree t : members) {
+			if (t.is(Tree.Kind.METHOD)) {
+				MethodTree mt = (MethodTree) t;
+				BlockTree bt = mt.block();
+				List<StatementTree> trees = bt.body();
+				for (StatementTree target : trees) {
+					if (target.is(Tree.Kind.RETURN_STATEMENT)) {
+						ReturnStatementTree rst = (ReturnStatementTree) target;
+						ExpressionTree expt = rst.expression();
+						Type type = expt.symbolType();
+						System.out.println(rst.returnKeyword().text());
+						
+					}
+				}
 			}
 		}
-//		if(methodNum == 0) {
-//			reportIssue(ct, "Class has 0 method");
+			
+		
+//		MethodTree mt = (MethodTree) tree;
+//		List<IdentifierTree> trees = mt.symbol().usages();
+//		System.out.println("use:" + mt.symbol().usages());
+//		System.out.println("num:" + trees.size());
+//		for (Tree target : trees) {
+//			while (target.parent() != null) {
+//				
+//				if(target.parent().is(Tree.Kind.CLASS)) {
+//					System.out.println("use in:" + target.parent());
+//					break;
+//				} else {
+//					target = target.parent();
+//				}		
+//			}
 //		}
+
 	}
 
 }
