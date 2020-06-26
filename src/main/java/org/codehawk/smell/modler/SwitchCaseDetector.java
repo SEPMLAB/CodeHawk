e5e320e68533eb2b295770d53916a0bc6eda2360
Yolok <jkluio785636@gmail.com>
2020-06-27 03:37:19 +0800

    Update SwitchCaseDetector.java

--- src/main/java/org/codehawk/smell/modler/SwitchCaseDetector.java
+++ src/main/java/org/codehawk/smell/modler/SwitchCaseDetector.java
@@ -8,35 +8,34 @@ import org.sonar.plugins.java.api.tree.CaseGroupTree;
 import org.sonar.plugins.java.api.tree.SwitchStatementTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
-public class SwitchCaseDetector extends BaseTreeVisitor implements Detector{
-	private final int maxCases = 10;
+public class SwitchCaseDetector extends BaseTreeVisitor implements Detector {
+	private static final int maxCases = 10;
 	List<SwitchNode> smellNodes = new ArrayList<>();
+
 	@Override
 	public boolean detect(Node node) {
-		if(smellNodes.contains(node))
-			return true;
-		
-		return false;
+		return smellNodes.contains(node);
 	}
-	
+
 	public void scanTrees(List<? extends Tree> trees) {
 		super.scan(trees);
 	}
-	
+
 	@Override
 	public void visitSwitchStatement(SwitchStatementTree tree) {
-		if(countSwitch(tree) >= maxCases)
+		if (countSwitch(tree) >= maxCases)
 			smellNodes.add(new SwitchNode(tree.openBraceToken().line()));
-		
+
 		super.visitSwitchStatement(tree);
 	}
-	
-	private int countSwitch(SwitchStatementTree switchStatementTree) {//count how many cases in this switch statement method 
+
+	private int countSwitch(SwitchStatementTree switchStatementTree) {// count how many cases in this switch statement
+																		// method
 		List<CaseGroupTree> caseGroupTreeList = switchStatementTree.cases();
 		return caseGroupTreeList.size();
 	}
-	
+
 	public List<SwitchNode> getSmellNodes() {
 		return smellNodes;
-	}	
+	}
 }
