983cb7663ce44e32354d46256df748ccc19187f6
jason30704 <52276669+jason30704@users.noreply.github.com>
2020-07-03 21:59:08 +0800

    fix 亂碼

--- src/main/java/org/codehawk/plugin/java/checks/AvoidInappropriateIntimacy.java
+++ src/main/java/org/codehawk/plugin/java/checks/AvoidInappropriateIntimacy.java
@@ -39,8 +39,8 @@ public class AvoidInappropriateIntimacy extends IssuableSubscriptionVisitor {
 	@Override
 	public void visitNode(Tree tree) {
 
-		ArrayList<MethodTree> vtUseList = new ArrayList<>();// ¬ö¿ıClass¤¤ªºPrivateÅÜ¼Æ³Q­ş¨ÇMETHOD¨Ï¥Î
-		ArrayList<String> mtUseList = new ArrayList<>(); // ¬ö¿ıvtUseList¤¤ªº¤èªk³Q­ş¨ÇClass¨Ï¥Î
+		ArrayList<MethodTree> vtUseList = new ArrayList<>();// ç´€éŒ„Classä¸­çš„Privateè®Šæ•¸è¢«å“ªäº›METHODä½¿ç”¨
+		ArrayList<String> mtUseList = new ArrayList<>(); // ç´€éŒ„vtUseListä¸­çš„æ–¹æ³•è¢«å“ªäº›Classä½¿ç”¨
 
 		// to count the number of classTrees
 		if (classCount == 0) {
@@ -51,7 +51,7 @@ public class AvoidInappropriateIntimacy extends IssuableSubscriptionVisitor {
 		String className = ct.simpleName().name();
 		int classLine = ct.openBraceToken().line();
 
-		// ¨ú±oPrivateÅÜ¼Æ¡A¨Ã°õ¦ævtuseCheck()
+		// å–å¾—Privateè®Šæ•¸ï¼Œä¸¦åŸ·è¡ŒvtuseCheck()
 		for (Tree t : ct.members()) {
 			if (t.is(Tree.Kind.VARIABLE)) {
 				VariableTree vt = (VariableTree) t;
@@ -87,7 +87,7 @@ public class AvoidInappropriateIntimacy extends IssuableSubscriptionVisitor {
 		}
 	}
 
-	// ¬ö¿ıClass¤¤ªºPrivateÅÜ¼Æ³Q­ş¨ÇMETHOD¨Ï¥Î
+	// ç´€éŒ„Classä¸­çš„Privateè®Šæ•¸è¢«å“ªäº›METHODä½¿ç”¨
 	public void vtuseCheck(List<IdentifierTree> trees, ArrayList<MethodTree> vtuseList) {
 		for (Tree target : trees) {
 			while (target.parent() != null) {
@@ -106,7 +106,7 @@ public class AvoidInappropriateIntimacy extends IssuableSubscriptionVisitor {
 		}
 	}
 
-	// ¬ö¿ıvtUseList¤¤ªº¤èªk³Q¨º¨ÇClass¨Ï¥Î¨ì
+	// ç´€éŒ„vtUseListä¸­çš„æ–¹æ³•è¢«é‚£äº›Classä½¿ç”¨åˆ°
 	public void rlListUpdate(ArrayList<MethodTree> vtuseList, String className, ArrayList<String> mtUseList) {
 
 		for (MethodTree mt : vtuseList) {
@@ -120,7 +120,7 @@ public class AvoidInappropriateIntimacy extends IssuableSubscriptionVisitor {
 						System.out.println("use in:" + ct.simpleName().name());
 						if (!(ct.simpleName().name()).equals(className)) {
 							mtUseList.add(ct.simpleName().name());
-							System.out.println("¦¨¥\¦s¤JClass");
+							System.out.println("æˆåŠŸå­˜å…¥Class");
 						}
 						break;
 					} else {
@@ -131,10 +131,10 @@ public class AvoidInappropriateIntimacy extends IssuableSubscriptionVisitor {
 		}
 	}
 
-	// µ²ºâPrivateÅÜ¼Æ³Q¨Ï¥Îªº¦¸¼Æ
+	// çµç®—Privateè®Šæ•¸è¢«ä½¿ç”¨çš„æ¬¡æ•¸
 	public void finalCheck(String className, int classLine, ArrayList<String> mtUseList) {
 
-		ArrayList<String> temp = new ArrayList<>(); // ¬ö¿ı¨Ï¥Î3¦¸¥H¤WªºCLASS
+		ArrayList<String> temp = new ArrayList<>(); // ç´€éŒ„ä½¿ç”¨3æ¬¡ä»¥ä¸Šçš„CLASS
 
 		while (mtUseList.size() > 2) {
 			System.out.println("list size: " + mtUseList.size());
@@ -155,7 +155,7 @@ public class AvoidInappropriateIntimacy extends IssuableSubscriptionVisitor {
 					classList.add(className);
 					classLineList.add(classLine);
 				}
-				System.out.println("¶W¹L3­Ó");
+				System.out.println("è¶…é3å€‹");
 			}
 			if (num > 1) {
 				for (int i = position.size() - 1; i >= 0; i--) {
