fb489852a0b8135c1804bd78937c392d9e478dc4
Yolok <jkluio785636@gmail.com>
2020-07-07 21:10:13 +0800

    Update ImperativeAbstraction.java

--- src/main/java/org/codehawk/plugin/java/checks/ImperativeAbstraction.java
+++ src/main/java/org/codehawk/plugin/java/checks/ImperativeAbstraction.java
@@ -3,7 +3,6 @@ package org.codehawk.plugin.java.checks;
 import java.util.Collections;
 import java.util.List;
 import org.codehawk.smell.modler.ImperativeAbstractionDetector;
-import org.codehawk.smell.modler.ImperativeAbstractionNode;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.tree.ClassTree;
@@ -20,15 +19,14 @@ public class ImperativeAbstraction extends IssuableSubscriptionVisitor {
 	@Override
 	public void visitNode(Tree tree) {
 		ClassTree classTree = (ClassTree) tree;
-		ImperativeAbstractionNode node = new ImperativeAbstractionNode(classTree.openBraceToken().line(),
-				classTree.simpleName().name());
 		ImperativeAbstractionDetector detector = new ImperativeAbstractionDetector();
 		detector.detect(classTree);
 
 		Boolean b = detector.check();
 		if (Boolean.TRUE.equals(b)) {
-			addIssue(node.getStartLine(),
-					"Code smell \"Imperative Abstraction\" occurred in class \"" + node.getName() + "\" !");
+			int line = classTree.openBraceToken().line();
+			String className = classTree.simpleName().name();
+			addIssue(line, "Code smell \"Imperative Abstraction\" occurred in class \"" + className + "\" !");
 		}
 	}
 }
